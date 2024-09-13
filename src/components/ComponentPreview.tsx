a83c747111b10c93b46247cad6e541138c6def25
andrre-ls <43184224+andrre-ls@users.noreply.github.com>
2024-09-13 15:47:04 +0100

    fix: border-radius

--- src/components/ComponentPreview.tsx
+++ src/components/ComponentPreview.tsx
@@ -4,7 +4,7 @@ interface ComponentPreviewProps {
 
 export function ComponentPreview({ children }: ComponentPreviewProps) {
   return (
-    <div className="w-full h-[24rem] mt-6 rounded-[1.5rem] flex justify-center items-center nx-bg-primary-700/5 dark:nx-bg-primary-300/10">
+    <div className="w-full h-[24rem] mt-6 nx-rounded-xl flex justify-center items-center nx-bg-primary-700/5 dark:nx-bg-primary-300/10">
       {children}
     </div>
   );
