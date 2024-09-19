52fb53ad51b8f715a93bd10db3159e7e743d69a4
andrre-ls <43184224+andrre-ls@users.noreply.github.com>
2024-09-19 15:21:19 +0100

    feat: add cva

--- src/lib/tailwind.ts
+++ src/lib/tailwind.ts
@@ -1,6 +1,15 @@
+import { cva } from 'class-variance-authority';
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs)) || undefined;
+}
+
+export function cnva<T>(...props: Parameters<typeof cva<T>>) {
+  const styles = cva<T>(...props);
+
+  return (...variants: Parameters<typeof styles>) => {
+    return cn(styles(...variants));
+  };
 }
