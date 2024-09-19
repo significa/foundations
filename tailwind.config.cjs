430d9d37f4bc11ced1168483037e79aeaa764748
andrre-ls <43184224+andrre-ls@users.noreply.github.com>
2024-09-19 13:26:30 +0100

    docs: oklch url

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@ const plugin = require('tailwindcss/plugin');
 
 const colors = {
   // in oklch color space
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/oklch
   '--color-background': '97.72% 0 0',
   '--color-primary': '25% 0 0',
   '--color-accent': '87.99% 0.17372425704675545 92.92368848780228'
