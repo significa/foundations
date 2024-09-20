bdedda94e937da26d64685811496dc9319d67c1a
andrre-ls <43184224+andrre-ls@users.noreply.github.com>
2024-09-20 15:09:44 +0100

    feat: Skeleton component

--- src/components/PreviewBox.tsx
+++ src/components/PreviewBox.tsx
@@ -4,7 +4,7 @@ interface PreviewBoxProps {
 
 export function PreviewBox({ children }: PreviewBoxProps) {
   return (
-    <div className="w-full h-[24rem] mt-6 first:mt-0 nx-rounded-xl flex justify-center items-center nx-bg-primary-700/5 dark:nx-bg-primary-300/10">
+    <div className="relative w-full h-[24rem] mt-6 first:mt-0 nx-rounded-xl flex justify-center items-center nx-bg-primary-700/5 dark:nx-bg-primary-300/10">
       {children}
     </div>
   );
---
+++ src/components/foundations/Skeleton/examples/SkeletonArticle.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { Button } from 'components/foundations/Button';
+import { Skeleton } from 'components/foundations/Skeleton';
+
+export function SkeletonArticle() {
+  const [loaded, setLoaded] = useState<boolean>(false);
+
+  return (
+    <>
+      <div className="flex flex-col gap-2 text-primary">
+        {loaded && (
+          <>
+            <div className="text-xl font-semibold">Eggs, Eggs, Eggs!</div>
+            <div className="text-xs text-primary/70 font-medium max-w-[320px]">
+              Eggs: nature’s prepackaged protein pod, versatile enough for everything from breakfast
+              to, well, more breakfast. Crack one open, and you’ve got a perfectly balanced combo of
+              yolk and white—one part golden goodness, one part suspiciously tasteless goo.
+            </div>
+            <Button size="sm" variant="secondary" className="w-max mt-4">
+              Get Cracking!
+            </Button>
+          </>
+        )}
+        {!loaded && (
+          <>
+            <Skeleton className="font-xl w-[12em] h-[1.6em] rounded-sm" />
+            <Skeleton className="font-xl w-[320px] h-[5em] rounded-sm" />
+            <Skeleton className="mt-4 w-[90px] h-[32px] rounded-sm" />
+          </>
+        )}
+      </div>
+      <div className="absolute top-4 left-4">
+        <Button size="sm" onClick={() => setLoaded(!loaded)}>
+          {loaded ? 'Unload' : 'Load'}
+        </Button>
+      </div>
+    </>
+  );
+}
