21c43b0d6f852f11a8dc364f6acfcb1c048cf48b
andrre-ls <43184224+andrre-ls@users.noreply.github.com>
2024-09-23 17:52:46 +0100

    fix: remove unused imports

--- src/components/foundations/Checkbox/Checkbox.tsx
+++ src/components/foundations/Checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useRef, useEffect, useImperativeHandle } from 'react';
+import { forwardRef } from 'react';
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { cn } from 'lib/tailwind';
 
