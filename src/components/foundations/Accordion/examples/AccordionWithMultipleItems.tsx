8c27bb8160926f7bbabfb790c9331f398ab03d8b
João Gomes <joaogomes@significa.co>
2024-09-30 14:00:50 +0100

    feat(foundations/accordion): adds accordion docs and example

---
+++ src/components/foundations/Accordion/examples/AccordionWithMultipleItems.tsx
@@ -0,0 +1,26 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../Accordion';
+
+export function AccordionWithMultipleItems() {
+  return (
+    <Accordion type="multiple">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Is it unstyled?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It's unstyled by default, giving you freedom over the look and feel.
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Can it be animated?</AccordionTrigger>
+        <AccordionContent>
+          Yes! You can animate the Accordion with CSS or JavaScript.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+}
