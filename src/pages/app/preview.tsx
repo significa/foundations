04b0e8cde95492df67d56262f75e0c6f571ce863
andrre-ls <43184224+andrre-ls@users.noreply.github.com>
2024-11-11 11:54:34 +0000

    fix(app/preview): loading state

--- src/pages/app/preview.tsx
+++ src/pages/app/preview.tsx
@@ -4,15 +4,18 @@ import { Spinner } from '@/foundations/components/Spinner';
 import { usePseudoDynamicComponent } from '@/lib/hooks/usePseudoDynamicComponent';
 
 function Preview() {
-  const { query } = useRouter();
+  const { query, isReady } = useRouter();
   const path = Array.isArray(query.path) ? query.path[0] : query.path;
   const Component = usePseudoDynamicComponent(path);
 
   return (
     <div className="w-full h-full min-h-[--box-height] flex items-center justify-center">
-      <Suspense fallback={<Spinner />}>
-        <Component />
-      </Suspense>
+      {!isReady && <Spinner />}
+      {isReady && (
+        <Suspense fallback={<Spinner />}>
+          <Component />
+        </Suspense>
+      )}
     </div>
   );
 }
