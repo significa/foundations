83bc05fd0e1c706150f6ab14a1007628d78ed4d1
Pedro Brandão <pedro@significa.co>
2025-01-10 15:10:08 +0000

    expandable code wrapper

--- src/components/source-code.tsx
+++ src/components/source-code.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from "react";
+
 import path from "path";
 
 import { readFile } from "@/lib/fs";
@@ -5,16 +7,23 @@ import { readFile } from "@/lib/fs";
 import { Markdown } from "./markdown";
 import { ExpandableCode } from "./expandable-code";
 
-export const SourceCode = async ({ file }: { file: string }) => {
+interface SourceCodeProps {
+  file: string;
+  expandable?: boolean;
+}
+
+export const SourceCode = async ({ file, expandable }: SourceCodeProps) => {
   const code = await readFile(
     path.join(process.cwd(), "src", ...file.split("/"))
   );
 
   const lang = path.extname(file).slice(1);
 
+  const Wrapper = expandable ? ExpandableCode : Fragment;
+
   return (
-    <ExpandableCode>
+    <Wrapper>
       <Markdown>{`\`\`\`${lang}\n${code}\`\`\``}</Markdown>
-    </ExpandableCode>
+    </Wrapper>
   );
 };
