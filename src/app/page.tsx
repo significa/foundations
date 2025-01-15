94fcb8ca888f2602389280490a9f6c4626f0050a
Pedro Brandão <pedro@significa.co>
2025-01-15 18:52:14 +0000

    fix redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-"use client"
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/setup');
-  }, [router]);
-
-  return (
-    <>
-      <div>Redirecting...</div>
-    </>
-  );
+  redirect("/setup");
 }
