7f2ed721748567d3ac5ea0b1c3eba7c5e1f868d0
Pedro Brandão <pedro@significa.co>
2025-01-16 22:35:27 +0000

    description

--- src/app/(docs)/header.tsx
+++ src/app/(docs)/header.tsx
@@ -8,16 +8,17 @@ import { GITHUB_REPO_URL } from "@/lib/constants";
 
 import { MobileNav } from "./mobile-nav";
 import { getNavigationWithDates } from "@/lib/navigation";
+import Link from "next/link";
 
 export const Header = async () => {
   return (
     <div className="border-border bg-background/95 sticky top-0 z-50 border-b backdrop-blur-sm">
       <div className="mx-auto flex h-12 max-w-screen-2xl items-center justify-between gap-2 px-2 md:h-14 md:px-4">
-        <div className="flex items-center gap-1.5">
+        <Link href="/" className="flex items-center gap-1.5">
           <MobileNav items={await getNavigationWithDates()} />
           <Egg />
           <div className="font-medium">Foundations</div>
-        </div>
+        </Link>
         <div className="flex items-center gap-1.5">
           <Button variant="ghost" size="sm" square asChild>
             <a href={GITHUB_REPO_URL} target="_blank" rel="noopener noreferrer">
