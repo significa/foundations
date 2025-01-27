fb96cef506626b0b4194468bcd3a7bc35b67191f
Pedro Brandão <pedro@significa.co>
2025-01-27 10:40:16 +0000

    ui host

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -13,6 +13,7 @@ export const PostHogProvider = ({
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+      ui_host: "https://eu.posthog.com",
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
       autocapture: true,
       persistence: "localStorage",
