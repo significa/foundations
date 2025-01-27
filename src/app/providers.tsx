c9d37f8b2801fba82b85085f0c3464e2fd3aa1cc
Francisco Marques <tofran@users.noreply.github.com>
2025-01-27 17:28:36 +0000

    remove posthog conditionals

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -10,26 +10,22 @@ export const PostHogProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
-  const { NEXT_PUBLIC_POSTHOG_KEY, NEXT_PUBLIC_POSTHOG_HOST } = process.env;
-
   useEffect(() => {
-    if (NEXT_PUBLIC_POSTHOG_KEY && NEXT_PUBLIC_POSTHOG_HOST) {
-      posthog.init(NEXT_PUBLIC_POSTHOG_KEY, {
-        api_host: NEXT_PUBLIC_POSTHOG_HOST,
-        ui_host: "https://eu.posthog.com",
-        capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-        autocapture: true,
-        persistence: "localStorage",
-        mask_all_text: true,
-        enable_heatmaps: true,
-        capture_pageleave: true,
-        person_profiles: "always",
-        loaded: (posthog) => {
-          if (process.env.NODE_ENV === "development") posthog.debug();
-        },
-      });
-    }
-  }, [NEXT_PUBLIC_POSTHOG_KEY, NEXT_PUBLIC_POSTHOG_HOST]);
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+      ui_host: "https://eu.posthog.com",
+      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+      autocapture: true,
+      persistence: "localStorage",
+      mask_all_text: true,
+      enable_heatmaps: true,
+      capture_pageleave: true,
+      person_profiles: "always",
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === "development") posthog.debug();
+      },
+    });
+  }, []);
 
   return (
     <PHProvider client={posthog}>
