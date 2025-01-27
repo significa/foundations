9a5c8bb002c848ca6d31aefa0623de14fdefa715
Tiago Duarte <tiago@coletiv.com>
2025-01-27 10:36:56 +0000

    feat(analytics): config posthog to be gdpr compliant

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -13,7 +13,16 @@ export const PostHogProvider = ({
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
-      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+      capture_pageview: false, // Disable automatic pageview capture, as we capture manually      
+      autocapture: true,
+      persistence: 'localStorage',
+      mask_all_text: true,
+      enable_heatmaps: true,     
+      capture_pageleave: true,        
+      person_profiles: 'always',
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === 'development') posthog.debug()
+      },          
     });
   }, []);
 
