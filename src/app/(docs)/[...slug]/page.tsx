3dc25a994daadf935116f6445e1b64befc90a5a6
Pedro Brandão <pedro@significa.co>
2025-02-18 15:48:45 +0000

    fix height

--- src/app/(docs)/[...slug]/page.tsx
+++ src/app/(docs)/[...slug]/page.tsx
@@ -52,7 +52,7 @@ export default async function Page({
 
   return (
     <>
-      <nav className="sticky top-14 order-last hidden h-[calc(100dvh_-_--spacing(14))] w-[200px] shrink-0 overflow-y-auto pt-6 text-sm lg:block">
+      <nav className="sticky top-14 order-last hidden h-[calc(100dvh-var(--spacing)*14)] w-[200px] shrink-0 overflow-y-auto pt-6 text-sm lg:block">
         {(toc.length > 0 ||
           (metadata.dependencies && metadata.dependencies.length > 0) ||
           (metadata.files && metadata.files.length > 0)) && (
