561d2e457253e5abcfe1edf99ca43535528330f8
Pedro Lunet <pedrolunet@gmail.com>
2025-02-28 12:26:28 +0000

    fix: used a new approach by adding the prefix "position:"

--- src/foundations/ui/select/select.tsx
+++ src/foundations/ui/select/select.tsx
@@ -21,7 +21,7 @@ const Select = ({ className, invalid, variant, ...props }: SelectProps) => {
       data-invalid={invalid}
       className={cn(
         inputStyle({ variant }),
-        "appearance-none bg-[length:1em] bg-[center_right_0.75rem] bg-no-repeat pr-10",
+        "appearance-none bg-[length:1em] bg-[position:right_--spacing(2)_center] bg-no-repeat pr-10",
         'bg-[url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDE2IDE2Ij48cGF0aCBmaWxsPSJibGFjayIgZD0iTTMuNyA1LjNsNC4zIDQuMyA0LjMtNC4zLjcuNy01IDUtNS01eiIvPjwvc3ZnPg==")]',
         'dark:bg-[url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDE2IDE2Ij48cGF0aCBmaWxsPSJ3aGl0ZSIgZD0iTTMuNyA1LjNsNC4zIDQuMyA0LjMtNC4zLjcuNy01IDUtNS01eiIvPjwvc3ZnPg==")]',
         className
