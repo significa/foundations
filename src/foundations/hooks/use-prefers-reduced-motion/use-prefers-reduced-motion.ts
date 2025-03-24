fb0cbc528bb0a701cad2b9f084ca1fadff980f00
Olusegun Omilabu <omilabuolusegun@gmail.com>
2025-03-24 12:48:38 +0000

    remove react import

--- src/foundations/hooks/use-prefers-reduced-motion/use-prefers-reduced-motion.ts
+++ src/foundations/hooks/use-prefers-reduced-motion/use-prefers-reduced-motion.ts
@@ -1,14 +1,14 @@
 "use client";
 
-import React from "react";
+import { useEffect, useState } from "react";
 
 const QUERY = "(prefers-reduced-motion: no-preference)";
 
 const usePrefersReducedMotion = (): boolean => {
   const [prefersReducedMotion, setPrefersReducedMotion] =
-    React.useState<boolean>(false);
+    useState<boolean>(false);
 
-  React.useEffect(() => {
+  useEffect(() => {
     const mediaQueryList = window.matchMedia(QUERY);
 
     const listener = (event: MediaQueryListEvent) => {
