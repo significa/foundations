f805a0f62874360f58b3d0e89caa6ee09831e183
kaaps <joao.guimaraes.ribeiro@gmail.com>
2025-05-23 09:52:30 +0100

    fix: lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,8 @@ import { PostHogProvider } from "./providers";
 
 export const metadata: Metadata = {
   title: "Foundations by Significa - A base config for digital products.",
-  description: "Explore Significa Foundations: our design and development principles, methodologies, and tools that ensure every digital product we create is purposeful, accessible, and beautifully built.",
+  description:
+    "Explore Significa Foundations: our design and development principles, methodologies, and tools that ensure every digital product we create is purposeful, accessible, and beautifully built.",
 };
 
 export default async function RootLayout({
