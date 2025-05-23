9fee1b3b2ea0b973145d1da220ed82a799962c30
kaaps <joao.guimaraes.ribeiro@gmail.com>
2025-05-23 09:46:39 +0100

    fix: title description og fav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { ThemeProvider } from "next-themes";
 import { PostHogProvider } from "./providers";
 
 export const metadata: Metadata = {
-  title: "Foundations",
-  description: "Significa documentation for building applications",
+  title: "Foundations by Significa - A base config for digital products.",
+  description: "Explore Significa Foundations: our design and development principles, methodologies, and tools that ensure every digital product we create is purposeful, accessible, and beautifully built.",
 };
 
 export default async function RootLayout({
