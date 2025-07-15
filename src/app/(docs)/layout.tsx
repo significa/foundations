fc6b1b69a2285a8960dd4979555f34868f803563
andrre-ls <43184224+andrre-ls@users.noreply.github.com>
2025-07-15 09:26:28 +0100

    fix lint

--- src/app/(docs)/layout.tsx
+++ src/app/(docs)/layout.tsx
@@ -18,9 +18,9 @@ export default async function DocsLayout({
           // controlled by button by ./header.tsx component
           data-mobile-menu-open="false"
           className={cn(
-            "bg-background shrink-0 overflow-y-auto overscroll-contain px-2 md:px-4 xl:px-2 pt-2 pb-12",
-            "fixed top-12 hidden h-[calc(100dvh-var(--spacing)*12)] w-full data-[mobile-menu-open=true]:block max-xl:z-200 mt-px",
-            "xl:sticky md:top-14 md:h-[calc(100dvh-var(--spacing)*14)] xl:max-w-[250px] xl:border-r xl:pt-4 xl:block"
+            "bg-background shrink-0 overflow-y-auto overscroll-contain px-2 pt-2 pb-12 md:px-4 xl:px-2",
+            "fixed top-12 mt-px hidden h-[calc(100dvh-var(--spacing)*12)] w-full data-[mobile-menu-open=true]:block max-xl:z-200",
+            "md:top-14 md:h-[calc(100dvh-var(--spacing)*14)] xl:sticky xl:block xl:max-w-[250px] xl:border-r xl:pt-4"
           )}
         >
           <Menu items={await getNavigationWithDates()} />
