d2a2179850918de812cb46fd982d327f80e31f15
Pedro Brandão <pedro@significa.co>
2025-08-08 15:39:30 +0100

    fix select style

--- src/foundations/guides/accessible-forms/examples/select.tsx
+++ src/foundations/guides/accessible-forms/examples/select.tsx
@@ -34,7 +34,7 @@ const Select = ({
       aria-labelledby={fieldCtx?.["aria-labelledby"]}
       className={cn(
         inputStyle({ variant }),
-        "appearance-none bg-[right_--spacing(2)_center] bg-size-[1em] bg-no-repeat pr-10",
+        "appearance-none bg-size-[1em] bg-position-[right_--spacing(2)_center] bg-no-repeat pr-10",
         'bg-[url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDE2IDE2Ij48cGF0aCBmaWxsPSJibGFjayIgZD0iTTMuNyA1LjNsNC4zIDQuMyA0LjMtNC4zLjcuNy01IDUtNS01eiIvPjwvc3ZnPg==")]',
         'dark:bg-[url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDE2IDE2Ij48cGF0aCBmaWxsPSJ3aGl0ZSIgZD0iTTMuNyA1LjNsNC4zIDQuMyA0LjMtNC4zLjcuNy01IDUtNS01eiIvPjwvc3ZnPg==")]',
         className
