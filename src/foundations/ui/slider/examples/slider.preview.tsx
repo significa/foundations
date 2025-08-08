a906672c7a7f22497c372fa279cab631a6903a77
Pedro Brandão <pedro@significa.co>
2025-08-08 18:54:40 +0100

    fix slider imports

--- src/foundations/ui/slider/examples/slider.preview.tsx
+++ src/foundations/ui/slider/examples/slider.preview.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import { Slider } from "../slider";
+import { Slider } from "@/foundations/ui/slider/slider";
 
 export default function SliderPreview() {
   return <Slider min={0} max={100} defaultValue={50} />;
