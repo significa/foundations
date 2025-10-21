91e53d384e4ec4621dc899406e9843e17d81a689
Nuno Craveiro <nunocraveiro@significa.co>
2025-10-21 15:14:38 +0100

    feat(pagefind): added dev script

--- scripts/pagefind.mjs
+++ scripts/pagefind.mjs
@@ -1,15 +1,21 @@
 import * as pagefind from "pagefind";
 
+const pagefindPath = process.env.ENVIRONMENT === "dev" ? ".next" : "out";
+const pagefindOutPath =
+  process.env.ENVIRONMENT === "dev" ? "public/pagefind" : "out/pagefind";
+
+console.log("Pagefind indexing directory: ", process.env.ENVIRONMENT);
+
 // Create a Pagefind search index to work with
 const { index } = await pagefind.createIndex();
 
 // Index all HTML files in a directory
 await index.addDirectory({
-  path: "out",
+  path: pagefindPath,
 });
 
 await index.writeFiles({
-  outputPath: "out/pagefind",
+  outputPath: pagefindOutPath,
 });
 
 await pagefind.close();
