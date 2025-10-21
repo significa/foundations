3cdf8ba135e5f5e3c579a49748e405395d309e00
Nuno Craveiro <nunocraveiro@significa.co>
2025-10-21 14:20:45 +0100

    feat(pagefind): test log

--- scripts/pagefind.mjs
+++ scripts/pagefind.mjs
@@ -2,11 +2,13 @@ import * as pagefind from "pagefind";
 
 const pagefindDir = process.env.NEXT_PUBLIC_PAGEFIND_DIR || ".next";
 
+console.log(`Pagefind indexing directory: `, pagefindDir);
+
 // Create a Pagefind search index to work with
 const { index } = await pagefind.createIndex();
 
 // Index all HTML files in a directory
-await index.addDirectory({
+await index.addDirectory({  
   path: pagefindDir,
 });
 
