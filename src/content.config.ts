902d273a738b5949a15359f5bf06e4f3a2d19553
andrre-ls <43184224+andrre-ls@users.noreply.github.com>
2026-03-16 14:35:17 +0000

    frontmatter source code and preview

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,24 +1,7 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, reference } from "astro:content";
 import { z } from "astro/zod";
 import { glob } from "astro/loaders";
 import { previewLoader } from "./lib/loaders";
-
-const pages = defineCollection({
-  loader: glob({
-    pattern: "**/page.mdx",
-    base: "./src/foundations",
-    generateId: ({ entry }) => entry.replace("/page.mdx", ""),
-  }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    meta: z
-      .object({
-        folder: z.string(),
-      })
-      .optional(),
-  }),
-});
 
 const previews = defineCollection({
   loader: previewLoader({
@@ -31,6 +14,25 @@ const previews = defineCollection({
   }),
   schema: z.object({
     file: z.string(),
+  }),
+});
+
+const pages = defineCollection({
+  loader: glob({
+    pattern: "**/page.mdx",
+    base: "./src/foundations",
+    generateId: ({ entry }) => entry.replace("/page.mdx", ""),
+  }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    preview: reference("previews").optional(),
+    files: z.array(z.string()).optional(),
+    meta: z
+      .object({
+        folder: z.string(),
+      })
+      .optional(),
   }),
 });
 
