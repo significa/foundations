f41de5f22e2ed42ccb8c029167d2904df2352ff7
andrre-ls <43184224+andrre-ls@users.noreply.github.com>
2026-03-16 15:04:59 +0000

    fix component-preview hydration

--- src/components/component-preview.tsx
+++ src/components/component-preview.tsx
@@ -1,27 +1,43 @@
-import { useMemo } from "react";
+import { Spinner } from "@/foundations/ui/spinner/spinner";
+import { useEffect, useState } from "react";
 
 type ComponentPreviewProps = {
   file: string;
 };
 
+const modules = import.meta.glob("/src/foundations/**/*.preview.tsx");
+
 const ComponentPreview = ({ file }: ComponentPreviewProps) => {
-  const modules = import.meta.glob("/src/foundations/**/*.preview.tsx", { eager: true });
+  const [Component, setComponent] = useState<React.ComponentType | null>(null);
 
-  const Component = useMemo(() => {
+  useEffect(() => {
     try {
-      // @ts-expect-error - dynamic import with glob
-      const module = modules[file]?.default;
-
-      if (!module) {
-        throw new Error(`Module not found: ${file}`);
-      }
-
-      return module;
+      modules[file]()
+        .then((mod) => {
+          // @ts-expect-error dynamic import is not typed
+          setComponent(() => mod.default);
+        })
+        .catch((error) => {
+          throw new Error(`Failed to load component for ${file}: ${error.message}`);
+        });
     } catch (error) {
-      console.error(`Error loading module: ${error}`);
-      return () => <div>Error loading component</div>;
+      console.error(`Error importing module for ${file}:`, error);
+      setComponent(() => () => (
+        <div className="space-y-2 p-4 text-sm text-red-700">
+          <p>Error loading component:</p>
+          <code className="text-foreground/60 bg-foreground/4 rounded-md border p-2 text-xs">{file}</code>
+        </div>
+      ));
     }
   }, [file]);
+
+  if (!Component) {
+    return (
+      <div className="flex size-full items-center justify-center p-2">
+        <Spinner />
+      </div>
+    );
+  }
 
   return <Component />;
 };
